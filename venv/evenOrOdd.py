03271120f48483d53ca6f2adc8a171e8a1412e12
sparsh0008 <54952775+sparsh0008@users.noreply.github.com>
2020-01-18 14:23:32 +0530

    Given number is even or odd

--- venv/evenOrOdd.py
+++ venv/evenOrOdd.py
@@ -2,9 +2,11 @@
 check a number is even or odd
 '''
 
-a = 11
+a = "3"
 
 if(a % 2 == 0):
     print("The number is even")
-else:
+elif(a % 2 != 0):
     print("The number is odd")
+else:
+    print("It is not a nuumber")

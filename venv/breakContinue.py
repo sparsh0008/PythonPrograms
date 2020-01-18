0176f0f27f0133aeffa97e773835161776b40232
sparsh0008 <54952775+sparsh0008@users.noreply.github.com>
2020-01-18 14:52:03 +0530

    break and continue in the loop

--- venv/breakContinue.py
+++ venv/breakContinue.py
@@ -1,8 +1,8 @@
-i = 0
+i = -1
 a = "Hello world"
-while(True):
-    if(a[i] == " "):
-        break
-    print(a[i])
+while(i < len(a)-1):
     i = i + 1
+    if(a[i] == "o"):
+        continue
+    print(a[i])
 print("Out of the while loop")

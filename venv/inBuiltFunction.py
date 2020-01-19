03cd5006033b308023eadf5ff157e3f448babe40
sparsh0008 <54952775+sparsh0008@users.noreply.github.com>
2020-01-19 13:49:59 +0530

    Using FILTER function and LAMBDA function simultaneously

--- venv/inBuiltFunction.py
+++ venv/inBuiltFunction.py
@@ -1,17 +1,19 @@
 '''
-FILTER In Built Function
-
-a = [1, 2, 3, 4, 5]
+FILTER function
+'''
+a = [0, 1, 2, 3, 4, 5]
 def even(a):
-    if(a % 2 == 0):
+    if(a % 2 != 0):
         return a
 b = list(filter(even, a))
 print(b)
-'''
+
 '''
 Using FILTER and LAMBDA function simultaneously
 '''
-a = [1, 2, 3, 4, 5]
 
-c = list(filter(lambda a : a % 2 == 0,a))
+a = [0, 1, 2, 3, 4, 5]
+
+c = list(filter(lambda a : a % 2 != 0,a))
 print(c)
+

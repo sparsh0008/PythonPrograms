3ff36e609cc14ce141dfd5ff3ff343ba6b41c8d4
sparsh0008 <54952775+sparsh0008@users.noreply.github.com>
2020-01-25 13:24:10 +0530

    Finding vowels in a user entered string using list comprehension

--- venv/vowel.py
+++ venv/vowel.py
@@ -2,5 +2,5 @@ char = input("enter a string : ")
 
 vowels = [ char[i] for i in range(len(char)) if (char[i] in ('a', 'e','i','o','u') )]
 
-print("No of Vowels in the string is ")
+print("No of Vowels in {0} is ".format(char))
 print(len(vowels))
